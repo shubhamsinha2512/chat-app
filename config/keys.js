21330b2d4ad43fb0e7dcdb7839a3e2f25a452b93
Shubham Sinha <shubham.sinha2512@gmail.com>
2021-01-04 10:59:30 +0530

    initial commit done | minor changes at backend

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://shubham:shubham@chat-app-db.x4ran.mongodb.net/chat-app-db?authSource=admin&replicaSet=atlas-qawng4-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true",
+  secretOrKey: "secret",
+};
